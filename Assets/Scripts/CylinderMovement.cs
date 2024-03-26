2c67350f57e1c383ea42a613cbbe2ed2a8b5cf4b
keoshek <atamyrat341@mail.ru>
2024-03-26 11:00:47 +0500

    Initial commit

---
+++ Assets/Scripts/CylinderMovement.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class CylinderMovement : MonoBehaviour
+{
+    private GameManager gameManagerScript;
+    // Scale variables
+    private Vector3 originalScale;
+    private Vector3 scaleTo;
+    // Move left variables
+    private Vector3 originalPosition;
+    private Vector3 moveTo;
+    private Vector3 zeroPosition = new Vector3(0, 0, 0);
+    private Vector3 onePosition = new Vector3(-0.4f, 0, 0);
+    [SerializeField] float moveDuration = 0.5f;
+    // Text Spawn
+    public Canvas scoreTextPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerScript = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        ScaleCylinder(gameManagerScript.scaleRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PressMouse();
+    }
+
+    void ScaleCylinder(float scaleRate)
+    {
+        originalScale = transform.localScale;
+        scaleTo = originalScale * scaleRate;
+        float scaleDuration = scaleRate / gameManagerScript.scaleTime; //scaleRate / 200;
+        transform.DOScaleY(scaleTo.y, scaleDuration).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo).SetId("scale");
+    }
+
+    void MoveLeft()
+    {
+        if (gameManagerScript.isGameOn)
+        {
+            originalPosition = transform.position;
+            moveTo = originalPosition + onePosition;
+            transform.DOMoveX(moveTo.x, moveDuration).OnComplete(() =>
+            {
+                TriggerGameOver();
+                SpawnAtOnePosition();
+            });
+            if (transform.position.x < -5)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+
+    void PressMouse()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            DOTween.Pause("scale");
+            if (transform.position.x == zeroPosition.x)
+            {
+                // Round Up the scale value to whole numbers
+                float round = transform.localScale.y * 100;
+                round = Mathf.Floor(round) / 100;
+                transform.DOScale(new Vector3(1, round, 1), 0.01f);
+                // Rescale cylinder if the value is near to previous cylinder
+                if (transform.localScale.y > (gameManagerScript.scaleRate - 3) / 100)
+                {
+                    transform.DOScale(new Vector3(1, gameManagerScript.scaleRate / 100, 1), 0.01f);
+                    gameManagerScript.powerUpCount++;
+                    SpawnText();
+                    if (gameManagerScript.powerUpCount > 2 && gameManagerScript.scaleRate <= 95)
+                    {
+                        gameManagerScript.scaleRate += 5;
+                        gameManagerScript.powerUpCount = 1;
+                        DeleteScoreText();
+                        transform.DOScale(new Vector3(1, gameManagerScript.scaleRate / 100, 1), 0.5f).OnComplete(() =>
+                        {
+                            SpawnText();
+                        });
+                    }
+                    else if (gameManagerScript.powerUpCount > 2 && gameManagerScript.scaleRate > 95)
+                    {
+                        gameManagerScript.scaleRate = 100;
+                        gameManagerScript.powerUpCount = 1;
+                        DeleteScoreText();
+                        transform.DOScale(new Vector3(1, gameManagerScript.scaleRate / 100, 1), 0.5f).OnComplete(() =>
+                        {
+                            SpawnText();
+                        });
+                    }
+                }
+                else
+                {
+                    float round2 = transform.localScale.y * 100;
+                    round2 = Mathf.Floor(round2);
+                    gameManagerScript.scaleRate = round2;
+                    gameManagerScript.powerUpCount = 1;
+                    SpawnText();
+                }
+                gameManagerScript.UpdateScore(5);
+            }
+            MoveLeft();
+        }
+    }
+
+    void SpawnAtOnePosition()
+    {
+        if (transform.position.x == onePosition.x)
+        {
+            gameManagerScript.CylinderSpawner();
+        }
+    }
+
+    void SpawnText()
+    {
+        Canvas newCanvas =
+        Instantiate(scoreTextPrefab, transform.position + new Vector3(0, transform.localScale.y * 2 + 0.1f, 0), scoreTextPrefab.transform.rotation) as Canvas;
+        newCanvas.transform.SetParent(transform);
+        newCanvas.GetComponentInChildren<TextMeshProUGUI>().text = gameManagerScript.scaleRate.ToString(); // round.ToString();
+    }
+
+    void DeleteScoreText()
+    {
+        Destroy(FindObjectOfType<Canvas>().gameObject);
+    }
+
+    void TriggerGameOver()
+    {
+        if (transform.localScale.y < 0.05f)
+        {
+            gameManagerScript.GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public Button easyButton;
+    public Button difficultButton;
+    [SerializeField] int score;
+    [SerializeField] int highScore;
+    public GameObject cylinderPrefab;
+    public float scaleRate = 100;
+    public float scaleTime;
+    public float scaleDuration;
+    public float powerUpCount = 0;
+    public bool isGameOn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleDuration = scaleRate / scaleTime;
+    }
+
+    public void StartGame(int difficulty)
+    {
+        easyButton.gameObject.SetActive(false);
+        difficultButton.gameObject.SetActive(false);
+        isGameOn = true;
+        scoreText.gameObject.SetActive(true);
+        highScoreText.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        score = 0;
+        scaleTime = difficulty;
+        UpdateScore(0);
+        CylinderSpawner();
+    }
+
+    public void CylinderSpawner()
+    {
+        if (isGameOn)
+        {
+            Instantiate(cylinderPrefab, transform.position, cylinderPrefab.transform.rotation);
+        }
+    }
+
+    public void GameOver()
+    {
+        isGameOn = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        SaveHighScore();
+    }
+
+    public void UpdateScore(int addScore)
+    {
+        score += addScore;
+        scoreText.text = "Score: " + score;
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("highScore");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void SaveHighScore()
+    {
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            if (score > PlayerPrefs.GetInt("highScore"))
+            {
+                highScore = score;
+                PlayerPrefs.SetInt("highScore", highScore);
+                PlayerPrefs.Save();
+            }
+        }
+        else
+        {
+            if (score > highScore)
+            {
+                highScore = score;
+                PlayerPrefs.SetInt("highScore", highScore);
+                PlayerPrefs.Save();
+            }
+        }
+    }
+}
